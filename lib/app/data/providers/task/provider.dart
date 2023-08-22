2e21e5766a2aa7d0ee6ccd425122eab095fbb590
heispapijay <heispapijay@gmail.com>
2023-08-22 11:31:20 +0300

    project setup

---
+++ lib/app/data/providers/task/provider.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:doltnow/app/core/utils/keys.dart';
+import 'package:doltnow/app/data/models/task.dart';
+import 'package:doltnow/app/data/services/storage/services.dart';
+import 'package:get/get.dart';
+
+class TaskProvider {
+  StorageService _storage = Get.find<StorageService>();
+
+// {
+//   'tasks': [
+//     {'title': 'Work',
+//     'color': '#FFC542',
+//     'icon': 0xe123'}
+//   ]
+// }
+  List<Task> readTasks() {
+    var tasks = <Task>[];
+    jsonDecode(_storage.read(taskKey).toString())
+        .forEach((e) => tasks.add(Task.fromJson(e)));
+    return tasks;
+  }
+
+  void writeTasks(List<Task> tasks) {
+    _storage.write(taskKey, jsonEncode(tasks));
+  }
+}
